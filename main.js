bc2fddb5ebee0f2831dd2bc9901fadc252622bfa
Anaka Steinmetz <anakasteinmetz@Anakas-MBP.wireless.newschool.edu>
2018-04-26 14:21:03 -0400

    ignore

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
 'use strict'
 
 const IMAGES = {
-	house: 'https://i.imgur.com/14luBmT.png'
+	house: 'https://i.imgur.com/3mtzoKb.png'
 };
 
 
-const GRID_ROWS = 10;
-const GRID_COLS= 10;
+const GRID_ROWS = 5;
+const GRID_COLS= 5;
 const GRID_CELL_SIZE = 100;
 const GRID_EMPTY = [247, 245, 165];
+const GRID_DRAG = false;
 
 const RESOURCES = {
 	Population: '🚶',
 	Money: '💵',
-  Houses: '🏠'
+ 	Houses: '🏠'
 }
 
 const STATE = {
 	resources: {
 		Population: 10,
     HomelessPopulation: 10,
-		Money: 1000,
-    Houses: 3
+	Money: 350,
+    Houses: 350
 	},
 	cashPerHouse: 350
 }
@@ -35,25 +36,40 @@ class Population extends Item{
   }
 }
 
-class House extends Item{
-  init(){
-    this.quantity = 3;
-    STATE.resources.HomelessPopulation -= 3;
-    STATE.resources.HomelessPopulation = Math.max(0, STATE.resources.HomelessPopulation)
-  }
-  get image() {
-  	return 'house'
-  }
+class House extends Item {
+	init() {
+		this.quantity = 3;
+	}
+	get cost() {
+		return {
+			Money: 350
+		}
+	}
+	get image() {
+		return 'house'
+	}
+
+
+	onPlace() {
+		STATE.Houses++;
+	}
 }
 
 function init() {
 	var house = new House();
-	GAME.grid.place(house, 0, 0);
+	GAME.grid.place(new House(), 0, 0);
+	GAME.grid.place(new House(), 1, 0);
+	GAME.grid.place(new House(), 2, 0);
+
+
 
 	var menu = new Menu('Shop', [
-		new Button('Buy House', House),
+		new Button('Buy House', House)
 		//new Button('Buy Cure', Cure)
 		]);
+		defineHarvester('Money', function() {
+			return STATE.resources.Houses;
+		}, 60000);
 
   var population = new Population(); {
     if (population.house == 3) {
@@ -66,11 +82,11 @@ function main() {
   background(255,255,255);
 }
 
-let ev = new Event('Natural Disaster', 'on no! an eathquake destroyed part of the city!',[
-	new Action('rebuild houses', {money: 1000}, () => {
-		STATE.Money -= 1000;
-	}),
-	new Action('Do nothing', () => {
-		STATE.HomelessPopulation += 15;
-	})
-	]);
+// let ev = new Event('Natural Disaster', 'on no! an eathquake destroyed part of the city!',[
+// 	new Action('rebuild houses', {money: 1000}, () => {
+// 		STATE.Money -= 1000;
+// 	}),
+// 	new Action('Do nothing', () => {
+// 		STATE.HomelessPopulation += 15;
+// 	})
+// 	]);
