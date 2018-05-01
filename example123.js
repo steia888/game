0133a5df4aed1141332dee1d3a0bfe8d4f93ef0d
Anaka Steinmetz <anakasteinmetz@Anakas-MBP.wireless.newschool.edu>
2018-05-01 14:03:14 -0400

    people and families

--- example123.js
+++ example123.js
@@ -1,7 +1,8 @@
 'use strict'
 
 const IMAGES = {
-	house: 'https://i.imgur.com/zRfNtG8.png'
+	house: 'https://i.imgur.com/zRfNtG8.png',
+  person: 'https://i.imgur.com/9ncGi3a.png'
 };
 
 
@@ -11,30 +12,51 @@ const GRID_CELL_SIZE = 100;
 const GRID_EMPTY = [247, 245, 165];
 const GRID_DRAG = false;
 
+
 const RESOURCES = {
-	Population: '🚶',
+	People: '🚶',
+	Families: '❤️',
 	Money: '💵',
-  Houses: '🏠'
+	Houses: '🏠'
 }
 
 const STATE = {
 	resources: {
-		Population: 10,
-    HomelessPopulation: 10,
+		People: 10,
+		Families: 10,
 		Money: 1000,
-    Houses: 3
+		Houses: 3
 	},
 	cashPerHouse: 350
 }
 
-class Population extends Item{
+class People extends Item{
   init(){
-    this.quantity = 10;
+    this.quantity = 3;
   }
-  get cost() {
-    return{}
+  get image() {
+    return 'person'
+  }
+  onClick() {
+    STATE.resources.Families += this.quantity;
+    STATE.resources.People -= this.quantity;
+    STATE.resources.Money += this.quantity;
+    every(function() {
+      place(Person, 3, 3)
+    },
+    30000);
+    every(function() {
+    	place(Person, 3, 4)
+    },
+    15000);
+    every(function() {
+    	place(Person, 3, 5)
+    },
+    17000);
+    this.destroy();
   }
 }
+
 
 class House extends Item{
   init(){
@@ -54,9 +76,18 @@ class House extends Item{
   }
 }
 
+// class HouseCell extends Cell {
+// 	get color() {
+// 		return [66, 134, 244];
+// 	}
+
+
+}
 function init() {
 	var house = new House();
 	GAME.grid.place(house, 0, 0);
+  var Person = new People();
+  GAME.grid.place(Person, 3, 2);
 
 	let menu = new Menu('Shop', [
 		new BuyButton('Buy House', House)
@@ -67,26 +98,22 @@ function init() {
     }, 60000);
 
 
-  var population = new Population(); {
-    if (population.house == 3) {
-      return 'This house is full'
-    }
-  }
+
+
+
+function main(){
+  background(255,255,255)
 }
 
 
 
-function main() {
-  background(255,255,255);
-}
-
-let ev = new Event('Natural Disaster', 'on no! an eathquake destroyed part of the city!',[
-	new Action('rebuild houses', {money: 1000}, () => {
-		STATE.Money -= 1000;
-	}),
-	new Action('Do nothing', () => {
-		STATE.HomelessPopulation += 15;
-	})
-	]);
+// let ev = new Event('Natural Disaster', 'on no! an eathquake destroyed part of the city!',[
+// 	new Action('rebuild houses', {money: 1000}, () => {
+// 		STATE.Money -= 1000;
+// 	}),
+// 	new Action('Do nothing', () => {
+// 		STATE.HomelessPopulation += 15;
+// 	})
+// 	]);
 
 
